86f53a94f9b35362c93bd50e153ac9cd3de8ccde
Kostis Gourgoulias <kgourgoulias@gmail.com>
2016-04-05 12:14:22 -0400

    corrections

--- variational-inference/examples/gaussian.R
+++ variational-inference/examples/gaussian.R
@@ -1,17 +1,18 @@
+# Simple example 1 
+
+
 setwd("~/Dropbox/UMassCourses/set-phasers-to-stan/variational-inference/examples/")
 
 library(ggplot2)
 library(rstan)
 
-# generate data following normal distribution
+# Normally distributed data
 N <- 1000
-
 y <- sqrt(2)*rnorm(N)+10
+
 
 m <- stan_model(file="gaussian.stan")
 f <- vb(m)
-
-print(f)
 
 params<-extract(f,pars=c("mu","sigma"))
 
@@ -21,5 +22,8 @@ inf_sigma <- mean(params$sigma)
 Y <- seq(inf_mu-2,inf_mu+2, length.out = 100 )
 Y_Gaussian_model <- dnorm(Y,mean=inf_mu, sd=inf_sigma)
 Y_Gaussian_exact <- dnorm(Y,mean=0, sd=1)
+
+print(f)
+
 
 ## Plot those above at the same graph
